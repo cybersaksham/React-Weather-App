ce1288ae0c1d51cb6c4021a3257f8f8caf362d25
Saksham <2020UCP1801@mnit.ac.in>
2021-09-10 14:15:03 +0530

    top searches

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 
 function App() {
+  const [searches, setSearches] = useState(
+    JSON.parse(sessionStorage.getItem("searches")) || []
+  );
+
+  useEffect(() => {
+    let newSearches = [];
+    if (Array.from(searches).length === 0)
+      newSearches = ["Jaipur", "Delhi", "Bombay"];
+    else if (Array.from(searches).length === 1)
+      newSearches = [...searches, "Jaipur", "Delhi"];
+    else if (Array.from(searches).length === 2)
+      newSearches = [...searches, "Jaipur"];
+    setSearches(newSearches);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Navbar />
+      <div className="container my-3"></div>
     </>
   );
 }
